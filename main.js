0fc217b3dad54e346cdecff68a575993a3242c89
Victor Elias <svvictorelias@gmail.com>
2022-05-10 10:20:47 -0300

    adicionado layout da nlw e criado contador dos resultados

---
+++ main.js
@@ -0,0 +1,87 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll(){
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section){
+  const targetLine = scrollY + innerHeight/2
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+  const sectionEndsAt = sectionTop +sectionHeight
+  const sectionEndPassesTargetLine = sectionEndsAt <= targetLine
+
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassesTargetLine
+  
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if(sectionBoundaries){
+    menuElement.classList.add('active')
+  }
+}
+
+function showNavOnScroll(){
+  if(scrollY>3){
+    navigation.classList.add('scroll')
+  }else{
+    navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll(){
+  if(scrollY>500){
+    backToTopButton.classList.add('show')
+  }else{
+    backToTopButton.classList.remove('show')
+  }
+}
+
+function openMenu(){
+  document.body.classList.add('menu-expanded')
+}
+function closeMenu(){
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+#home,
+#home img,
+#home .stats,
+#services,
+#services header,
+#services .card,
+#about,
+#about header,
+#about .content`);
+
+
+
+let valueDisplays = document.querySelectorAll('.numCount')
+let interval = 4000
+
+valueDisplays.forEach((valueDisplay)=>{
+  let startValue = 0
+  let endValue = parseInt(valueDisplay.getAttribute('data-val'))
+  let duration = Math.floor(interval/endValue)
+  let counter = setInterval(function(){
+    startValue += 1
+    valueDisplay.textContent = `+${startValue}`
+    if(startValue == endValue){
+      clearInterval(counter)
+    }
+  }, duration)
+  console.log(endValue)
+})
